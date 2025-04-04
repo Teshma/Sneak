3168b7fddd8ad5ffaf1b630eefaa07b7100aede4
teshma <teshma17@gmail.com>
2025-04-04 22:24:11 +0100

    Wiping back to a clean state, things got too overcomplicated - lets try again and only abstract as and when I need to. The goal should be making it easy to come back to if I take a break

--- main.lua
+++ main.lua
@@ -6,43 +6,14 @@ debug = false
 function love.load()
     Width, Height = love.graphics.getDimensions()
 
-    local playerData =
-    {
-        x = 100,
-        y = 100,
-        w = 32,
-        h = 32,
-        speed = 128,
-        colour = {1, 0, 1, 1}
-    }
-
-    Player:Initialise(playerData)
-
-    local enemyData =
-    {
-        x = Width/2,
-        y = Height/2,
-        w = 32,
-        h = 32,
-        speed = 128,
-        colour = {0, 0, 1, 1},
-        direction = {x = 0, y = -1},
-        sightRange = 150,
-    }
-
-    Enemies:Create(enemyData)
+    
 end
 
 function love.update(dt)
-    Player:Update(dt)
-    Enemies:Update(Player, dt)
-    StateMachines:Update()
-
-    Renderers:Update(dt)
+    
 end
 
 function love.draw()
-    Renderers:Render()
     love.graphics.print(love.timer.getFPS(), 0, 0)
 end
 
